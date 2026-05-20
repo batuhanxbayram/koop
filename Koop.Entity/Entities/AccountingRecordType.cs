31a80832e746a49e55201de880448881a9669b73
Batuhan <144462287+batuhanxbayram@users.noreply.github.com>
2026-05-20 17:04:38 +0300

    Add accounting record type enum

---
+++ Koop.Entity/Entities/AccountingRecordType.cs
@@ -0,0 +1,11 @@
+namespace Koop.Entity.Entities
+{
+    public enum AccountingRecordType
+    {
+        Income = 1,
+        Expense = 2,
+        Payment = 3,
+        OpeningBalance = 4,
+        Adjustment = 5
+    }
+}
