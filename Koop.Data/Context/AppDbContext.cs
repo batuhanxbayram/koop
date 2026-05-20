47e1f65ea9abd1b32975b5db03012a5369987dd3
Batuhan <144462287+batuhanxbayram@users.noreply.github.com>
2026-05-20 17:04:06 +0300

    Add accounting records to DbContext

--- Koop.Data/Context/AppDbContext.cs
+++ Koop.Data/Context/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Koop.Entity.Entities;
+using Koop.Entity.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +18,7 @@ namespace Koop.Data.Context
         public DbSet<Route> Routes { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<RouteVehicleQueue> RouteVehicleQueues { get; set; }
+        public DbSet<AccountingRecord> AccountingRecords { get; set; }
 
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
@@ -42,6 +43,21 @@ namespace Koop.Data.Context
             builder.Entity<RouteVehicleQueue>()
                 .HasIndex(q => new { q.RouteId, q.VehicleId })
                 .IsUnique();
+
+            builder.Entity<AccountingRecord>()
+                .HasOne(a => a.Vehicle)
+                .WithMany(v => v.AccountingRecords)
+                .HasForeignKey(a => a.VehicleId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<AccountingRecord>()
+                .HasOne(a => a.CreatedByUser)
+                .WithMany()
+                .HasForeignKey(a => a.CreatedByUserId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<AccountingRecord>()
+                .HasIndex(a => new { a.VehicleId, a.Date });
 
             
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
